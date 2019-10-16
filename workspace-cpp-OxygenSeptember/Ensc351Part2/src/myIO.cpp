5024f5a5c7416652b73f04cc968b38d10bd54504
osboxes.org <osboxes@osboxes>
2019-10-15 20:09:23 -0700

    created Utility class

--- workspace-cpp-OxygenSeptember/Ensc351Part2/src/myIO.cpp
+++ workspace-cpp-OxygenSeptember/Ensc351Part2/src/myIO.cpp
@@ -7,14 +7,26 @@
 #include <unistd.h>			// for read/write/close
 #include <fcntl.h>			// for open/creat
 #include <sys/socket.h> 		// for socketpair
+#include <thread> //for testing
+#include "myIO.h"
 #include "SocketReadcond.h"
 #include <mutex>
 #include <condition_variable>
+#include "AtomicCOUT.h"
+#include <vector>
 
-std::mutex mut;
-std::condition_variable cv;
+class Utility{
+public:
+    std::mutex mut;
+    std::condition_variable cv;
+    int count = 0;
+    int spair = 0;
 
-int myReadcond(int des, void * buf, int n, int min, int time, int timeout);
+};
+
+std::vector<Utility> socketpairs(3);
+
+Utility ins;
 
 int mySocketpair( int domain, int type, int protocol, int des[2] )
 {
@@ -34,15 +46,37 @@ int myCreat(const char *pathname, mode_t mode)
 
 ssize_t myRead( int fildes, void* buf, size_t nbyte )
 {
+    //if(ins.count !=)
+
+    COUT << "Thread " << std::this_thread::get_id() << "getting Read mutex" << std::endl;
+    std::lock_guard<std::mutex> lk(ins.mut);
     // deal with reading from descriptors for files
     // myRead (for our socketpairs) reads a minimum of 1 byte
-    return myReadcond(fildes, buf, (int)nbyte, 1, 0, 0);
+    int testbyteRead = myReadcond(fildes, buf, (int)nbyte, 1, 0, 0);
+    if (testbyteRead != -1){
+        ins.count = ins.count - testbyteRead;
+        COUT << "after reading: " << ins.count << std::endl;
+        if(ins.count == 0){
+            ins.cv.notify_one();
+        }
+    }
+
+    return testbyteRead;
 	//return read(fildes, buf, nbyte );
 }
 
 ssize_t myWrite( int fildes, const void* buf, size_t nbyte )
 {
-	return write(fildes, buf, nbyte );
+    COUT << "Thread " << std::this_thread::get_id() << "getting Write mutex" << std::endl;
+    std::lock_guard<std::mutex> lk(ins.mut);
+    int testbyteWrite = write(fildes, buf, nbyte );
+    if(testbyteWrite != -1){
+        ins.count = ins.count + testbyteWrite;
+
+        COUT << "after writing: " << ins.count << std::endl;
+    }
+
+	return testbyteWrite;
 }
 
 int myClose( int fd )
@@ -52,6 +86,9 @@ int myClose( int fd )
 
 int myTcdrain(int des)
 { //is also included for purposes of the course.
+    std::unique_lock<std::mutex> lk(ins.mut);
+    ins.cv.wait(lk, []{return (ins.count==0);});
+
 	return 0;
 }
 
