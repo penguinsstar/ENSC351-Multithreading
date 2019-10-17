8e5a424920ee3374294a2858b8385760ab156594
osboxes.org <osboxes@osboxes>
2019-10-17 12:13:03 -0700

    vector of pointers

--- workspace-cpp-OxygenSeptember/Ensc351Part2/src/myIO.cpp
+++ workspace-cpp-OxygenSeptember/Ensc351Part2/src/myIO.cpp
@@ -7,13 +7,14 @@
 #include <unistd.h>			// for read/write/close
 #include <fcntl.h>			// for open/creat
 #include <sys/socket.h> 		// for socketpair
+#include "SocketReadcond.h"
 #include <thread> //for testing
 #include "myIO.h"
-#include "SocketReadcond.h"
 #include <mutex>
 #include <condition_variable>
 #include "AtomicCOUT.h"
 #include <vector>
+#include <algorithm>
 
 class Utility{
 public:
@@ -23,15 +24,23 @@ public:
     int spair = 0;
 
 };
+std::vector<Utility*> socketpairs;
 
-std::vector<Utility> socketpairs(3);
-
-Utility ins;
+//Utility ins;
 
 int mySocketpair( int domain, int type, int protocol, int des[2] )
 {
-	int returnVal = socketpair(domain, type, protocol, des);
-	return returnVal;
+    int returnVal = socketpair(domain, type, protocol, des);
+    COUT << "FD1: " << des[0] << std::endl;
+    COUT << "FD2: " << des[1] << std::endl;
+    if(returnVal == 0 && (socketpairs.size() < (uint8_t)std::max(des[0], des[1]))){
+        socketpairs.resize(std::max(des[0], des[1])+1, nullptr);
+    }
+    socketpairs.at(des[0]) = new Utility;
+    socketpairs.at(des[0])->spair = des[1];
+    socketpairs.at(des[1]) = new Utility;
+    socketpairs.at(des[1])->spair = des[0];
+    return returnVal;
 }
 
 int myOpen(const char *pathname, int flags, mode_t mode)
@@ -48,16 +57,16 @@ ssize_t myRead( int fildes, void* buf, size_t nbyte )
 {
     //if(ins.count !=)
 
-    COUT << "Thread " << std::this_thread::get_id() << "getting Read mutex" << std::endl;
-    std::lock_guard<std::mutex> lk(ins.mut);
+//    COUT << "Thread " << std::this_thread::get_id() << "getting Read mutex" << std::endl;
+    std::lock_guard<std::mutex> lk(socketpairs.at(fildes)->mut);
     // deal with reading from descriptors for files
     // myRead (for our socketpairs) reads a minimum of 1 byte
     int testbyteRead = myReadcond(fildes, buf, (int)nbyte, 1, 0, 0);
     if (testbyteRead != -1){
-        ins.count = ins.count - testbyteRead;
-        COUT << "after reading: " << ins.count << std::endl;
-        if(ins.count == 0){
-            ins.cv.notify_one();
+        socketpairs.at(fildes)->count = socketpairs.at(fildes)->count - testbyteRead;
+        COUT << "after reading: " << socketpairs.at(fildes)->count << " read " << testbyteRead << " at FD " << fildes << std::endl;
+        if(socketpairs.at(fildes)->count == 0){
+            socketpairs.at(fildes)->cv.notify_one();
         }
     }
 
@@ -67,13 +76,13 @@ ssize_t myRead( int fildes, void* buf, size_t nbyte )
 
 ssize_t myWrite( int fildes, const void* buf, size_t nbyte )
 {
-    COUT << "Thread " << std::this_thread::get_id() << "getting Write mutex" << std::endl;
-    std::lock_guard<std::mutex> lk(ins.mut);
+//    COUT << "Thread " << std::this_thread::get_id() << "getting Write mutex" << std::endl;
+    std::lock_guard<std::mutex> lk(socketpairs.at(fildes)->mut);
     int testbyteWrite = write(fildes, buf, nbyte );
     if(testbyteWrite != -1){
-        ins.count = ins.count + testbyteWrite;
+        socketpairs.at(socketpairs.at(fildes)->spair)->count = socketpairs.at(socketpairs.at(fildes)->spair)->count + testbyteWrite;
 
-        COUT << "after writing: " << ins.count << std::endl;
+        COUT << "after writing: " << socketpairs.at(socketpairs.at(fildes)->spair)->count << " written " << testbyteWrite << " at FD " << fildes << std::endl;
     }
 
 	return testbyteWrite;
@@ -86,9 +95,10 @@ int myClose( int fd )
 
 int myTcdrain(int des)
 { //is also included for purposes of the course.
-    std::unique_lock<std::mutex> lk(ins.mut);
-    ins.cv.wait(lk, []{return (ins.count==0);});
-
+    std::unique_lock<std::mutex> lk(socketpairs.at(des)->mut);
+//    socketpairs.at(des)->cv.wait(lk, [](int des){return (socketpairs.at(des)->count==0);});
+    socketpairs.at(des)->cv.wait(lk, [&des]{return (socketpairs.at(des)->count==0); });
+    COUT << "TCDrain cond: " << (socketpairs.at(des)->count==0) << std::endl;
 	return 0;
 }
 
